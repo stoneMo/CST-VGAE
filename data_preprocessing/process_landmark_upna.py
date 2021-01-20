07d0b6a37a2b7fe358c5f704b1eb8f4e4c4c58c2
ShentongMo <shentonm@andrew.cmu.edu>
2021-01-20 21:24:28 +0800

    fix

--- data_preprocessing/process_landmark_upna.py
+++ data_preprocessing/process_landmark_upna.py
@@ -30,24 +30,24 @@ def get_args():
 
 def find_19_points_st_gcn(key_points):
     graph_points = []
-    # graph_points_list = [1, 3, 8, 13, 15, 17, 21, 22, 26, 31, 33, 35, 36,
-    #                      39, 42, 45, 48, 51, 54]
+    graph_points_list = [1, 3, 8, 13, 15, 17, 21, 22, 26, 31, 33, 35, 36,
+                         39, 42, 45, 48, 51, 54]
 
     for i in range(len(key_points)):
-        # if i in graph_points_list:
-        x = key_points[i][0]
-        y = key_points[i][1]
-        graph_points.append(x)
-        graph_points.append(y)
+        if i in graph_points_list:
+            x = key_points[i][0]
+            y = key_points[i][1]
+            graph_points.append((x,y))
+            # graph_points.append(y)
 
     #print(graph_points)
 
-    #norm_graph_points = style_19_points(graph_points)
+    norm_graph_points = style_19_points(graph_points)
     #print(norm_graph_points)
 
-    data_numpy = np.zeros((2, 68))
-    data_numpy[0, :] = graph_points[0::2]
-    data_numpy[1, :] = graph_points[1::2]
+    data_numpy = np.zeros((2, 19))
+    data_numpy[0, :] = norm_graph_points[0::2]
+    data_numpy[1, :] = norm_graph_points[1::2]
 
     #print(data_numpy)
 
@@ -188,14 +188,14 @@ def main():
     max_angle = args.max_angle
     data = dict()
 
-    # extract all frames 
     for root, dirs, files in os.walk(dataset_path, topdown=True):
         for name in sorted(dirs):
             folder_index = int(name.split("_")[-1])
             # if int(folder_index) == 1:
             print(name)
             dir_path = os.path.join(root, name)
-            extract_all_frames(dir_path, name)
+            # extract all frames 
+            # extract_all_frames(dir_path, name)
             samples = dump_pyr_xyc(dir_path, max_angle, name)
             # print(len(samples.keys()))
             data.update(samples)
